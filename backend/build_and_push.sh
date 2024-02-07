16da075822b952b9fb51917997a7dc28df5ace86
lbjhk <libingjun1024@gmail.com>
2024-02-07 15:07:58 +0800

    [mod]update auto deploy script ECR repo uri for matching mishkan

--- backend/build_and_push.sh
+++ backend/build_and_push.sh
@@ -8,12 +8,12 @@ docker build --platform=linux/amd64 -t mishkan-backend:$version . --no-cache
 
 # 登錄到AWS ECR
 aws_login=$(aws ecr get-login-password --region ap-northeast-1)
-docker login -u AWS -p "${aws_login}" 767397998406.dkr.ecr.ap-northeast-1.amazonaws.com
+docker login -u AWS -p "${aws_login}" 417696335634.dkr.ecr.ap-northeast-1.amazonaws.com
 
 # 標記Docker映像
-docker tag mishkan-backend:$version 767397998406.dkr.ecr.ap-northeast-1.amazonaws.com/mishkan-backend:$version
+docker tag mishkan-backend:$version 417696335634.dkr.ecr.ap-northeast-1.amazonaws.com/mishkan-backend:$version
 
 # 推送映像到ECR
-docker push 767397998406.dkr.ecr.ap-northeast-1.amazonaws.com/mishkan-backend:$version
+docker push 417696335634.dkr.ecr.ap-northeast-1.amazonaws.com/mishkan-backend:$version
 
 echo "Docker image mishkan-backend:$version has been successfully pushed to AWS ECR."
--- frontend/build_and_push.sh
+++ frontend/build_and_push.sh
@@ -8,12 +8,12 @@ docker build --platform=linux/amd64 -t mishkan-frontend:$version . --no-cache
 
 # 登錄到AWS ECR
 aws_login=$(aws ecr get-login-password --region ap-northeast-1)
-docker login -u AWS -p "${aws_login}" 767397998406.dkr.ecr.ap-northeast-1.amazonaws.com
+docker login -u AWS -p "${aws_login}" 417696335634.dkr.ecr.ap-northeast-1.amazonaws.com
 
 # 標記Docker映像
-docker tag mishkan-backend:$version 767397998406.dkr.ecr.ap-northeast-1.amazonaws.com/mishkan:$version
+docker tag mishkan-backend:$version 417696335634.dkr.ecr.ap-northeast-1.amazonaws.com/mishkan-frontend:$version
 
 # 推送映像到ECR
-docker push 767397998406.dkr.ecr.ap-northeast-1.amazonaws.com/mishkan:$version
+docker push 417696335634.dkr.ecr.ap-northeast-1.amazonaws.com/mishkan-frontend:$version
 
 echo "Docker image mishkan-frontend:$version has been successfully pushed to AWS ECR."
