6b8dc5616012009a96595c49b8c3e946b29bfa3d
Whitney <whitney304327@gmail.com>
2024-09-04 22:19:19 +0800

    add trending artist music score query

--- backend/artist/routes.py
+++ backend/artist/routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, Blueprint
 from flask_cors import CORS
 from random import randint
-from models import main_db, general_db, campaign_db
+from models import main_db, general_db, campaign_db, spotify_week_db
 from bson.json_util import dumps
 from flask_restful import Resource, reqparse, Api
 
@@ -81,6 +81,27 @@ def get_campaign():
     except Exception as e:
         return str(e)
 
+@artist_api_bp.route('/spotify/weekly-top-songs/<country>', methods=['GET'])
+def get_spotify_weekly_top_songs(country):
+    try:
+        coll = spotify_week_db[country]
+        # print(coll)
+        results = coll.aggregate([
+            {"$sort": {"datetime": -1}},
+            {"$limit": 1},
+            {"$project": {
+                "_id": 0,
+                "datetime": "$datetime",
+                "year": "$year",
+                "month": "$month",
+                "day": "$day",
+                "week": "$week",
+                "weekly_top_songs": "$weekly_top_songs"
+            }}
+        ])
+        return dumps({'result': results})
+    except Exception as e:
+        print(str(e))
 
 class CampaignPackageDetail(Resource):
     def get(self):
@@ -191,3 +212,349 @@ class ArtistInfo(Resource):
             return jsonify(response)
         except Exception as e:
             return dumps({'err': str(e)})
+
+
+class ArtistPopularity(Resource):
+    """
+     Calculate artist popularity
+
+     return
+    """
+    def get_music_charts(self, country, week):
+        results = general_db.spotify_charts.aggregate([
+            {"$match": {
+                "country": "southkorea"
+            }},
+            {"$match": {"week": 35}},
+            #     {"$unwind": "$weekly_top_songs"},
+            {"$project": {
+                "_id": 0,
+                # spotify
+                "datetime": "$datetime",
+                "country": "$country",
+                "year": "$year",
+                "month": "$month",
+                "day": "$day",
+                "week": "$week",
+                "spotify_weekly_top_songs": "$weekly_top_songs",
+                #         "youtube_charts_id": "$youtube_charts._id",
+                #         "youtube_week": "$youtube_charts.week",
+                #         "youtube_weekly_top_songs": "$youtube_charts.weekly_top_songs"
+            }},
+            {"$unwind": "$spotify_weekly_top_songs"},
+            # calculate spotify score
+            {"$project": {
+                "datetime": "$datetime",
+                "year": "$year",
+                "month": "$month",
+                "day": "$day",
+                "week": "$week",
+                "country": "$country",
+                "sp_rank": {"$toInt": "$spotify_weekly_top_songs.rank"},
+                # check song title
+                "sp_song": "$spotify_weekly_top_songs.title",
+                "sp_artist_id": "$spotify_weekly_top_songs.artist_id",
+                "artist": {
+                    "$trim": {
+                        "input": "$spotify_weekly_top_songs.artist",
+                        "chars": ","
+                    }
+                }
+            }},
+            {"$addFields": {
+                "sp_score": {
+                    "$subtract": [
+                        {"$toInt": 201},
+                        {"$toInt": "$sp_rank"}
+                    ]
+                }
+            }},
+            # group by artist, get total score
+            {"$group": {
+                "_id": "$sp_artist_id",
+                "datetime": {"$first": "$datetime"},
+                "year": {"$first": "$year"},
+                "month": {"$first": "$month"},
+                "day": {"$first": "$day"},
+                "week": {"$first": "$week"},
+                "country": {"$first": "$country"},
+                "artist": {"$first": "$artist"},
+                "sp_score_list": {"$addToSet": "$sp_score"}
+            }},
+            {"$project": {
+                "sp_id": "$_id",
+                "datetime": "$datetime",
+                "year": "$year",
+                "month": "$month",
+                "day": "$day",
+                "week": "$week",
+                "country": "$country",
+                "artist": "$artist",
+                "sp_total_score": {
+                    "$reduce": {
+                        "input": "$sp_score_list",
+                        "initialValue": 0,
+                        "in": {"$add": [{"$toInt": "$$value"}, "$$this"]}
+                    }
+                }
+            }},
+            # lookup artist
+            {"$lookup": {
+                "from": "Artist",
+                "localField": "sp_id",
+                "foreignField": "MUSIC_PLATFORMS.ID",
+                "as": "artist_info"
+            }},
+            # return needed fields
+            {"$project": {
+                "sp_id": "$sp_id",
+                "datetime": "$datetime",
+                "year": "$year",
+                "month": "$month",
+                "day": "$day",
+                "week": "$week",
+                "country": "$country",
+                "artist": "$artist",
+                "sp_total_score": "$sp_total_score",
+                "artist_mid": {
+                    "$arrayElemAt": ["$artist_info.MID", 0]
+                },
+                "music_platforms": {
+                    "$arrayElemAt": ["$artist_info.MUSIC_PLATFORMS", 0]
+                },
+                "social_medias": {
+                    "$arrayElemAt": ["$artist_info.SOCIAL_MEDIAS", 0]
+                }
+            }},
+            # concat music_platforms & social_medias
+            {"$project": {
+                "sp_id": "$sp_id",
+                "datetime": "$datetime",
+                "year": "$year",
+                "month": "$month",
+                "day": "$day",
+                "week": "$week",
+                "country": "$country",
+                "artist": "$artist",
+                "sp_total_score": "$sp_total_score",
+                "mid": "$artist_mid",
+                "all": {
+                    "$concatArrays": ["$music_platforms", "$social_medias"]
+                }
+            }},
+            {"$project": {
+                "mid": "$mid",
+                "artist": "$artist",
+                "datetime": "$datetime",
+                "year": "$year",
+                "month": "$month",
+                "day": "$day",
+                "week": "$week",
+                "country": "$country",
+                "sp_total_score": "$sp_total_score",
+                "platforms": {
+                    "$reduce": {
+                        "input": "$all",
+                        "initialValue": {},
+                        "in": {
+                            "$mergeObjects": [
+                                "$$value",
+                                {
+                                    "$switch": {
+                                        "branches": [
+                                            {
+                                                "case": {"$eq": ["$$this.PLATFORM", "SPOTIFY"]},
+                                                "then": {"spotify": "$$this.ID"}
+                                            },
+                                            {
+                                                "case": {"$eq": ["$$this.PLATFORM", "INSTAGRAM"]},
+                                                "then": {"instagram": "$$this.USER"}
+                                            },
+                                            {
+                                                "case": {"$eq": ["$$this.PLATFORM", "YOUTUBE"]},
+                                                "then": {"youtube": "$$this.USER"}
+                                            },
+                                            {
+                                                "case": {"$eq": ["$$this.PLATFORM", "TWITTER"]},
+                                                "then": {"twitter": "$$this.USER"}
+                                            },
+                                            {
+                                                "case": {"$eq": ["$$this.PLATFORM", "TIKTOK"]},
+                                                "then": {"tiktok": "$$this.USER"}
+                                            },
+                                            {
+                                                "case": {"$eq": ["$$this.PLATFORM", "WEIBO"]},
+                                                "then": {"weibo": "$$this.USER"}
+                                            },
+                                            {
+                                                "case": {"$eq": ["$$this.PLATFORM", "BILIBILI"]},
+                                                "then": {"bilibili": "$$this.USER"}
+                                            },
+                                            {
+                                                "case": {"$eq": ["$$this.PLATFORM", "MELON"]},
+                                                "then": {"melon": "$$this.ID"}
+                                            },
+                                            {
+                                                "case": {"$eq": ["$$this.PLATFORM", "GENIE"]},
+                                                "then": {"genie": "$$this.ID"}
+                                            },
+                                            {
+                                                "case": {"$eq": ["$$this.PLATFORM", "QQ"]},
+                                                "then": {"qq": "$$this.ID"}
+                                            },
+                                        ], "default": {}
+                                    }}]
+                        }}
+                }
+            }},
+            # lookup youtube charts
+            {"$lookup": {
+                "from": "youtube_charts",
+                "localField": "week",
+                "foreignField": "week",
+                "as": "youtube_charts"
+            }},
+            {"$unwind": "$youtube_charts"},
+            {"$match": {"youtube_charts.country": "south korea"}},
+            {"$project": {
+                "mid": "$mid",
+                "artist": "$artist",
+                "datetime": "$datetime",
+                "year": "$year",
+                "month": "$month",
+                "day": "$day",
+                "week": "$week",
+                "country": "$country",
+                "sp_total_score": "$sp_total_score",
+                "platforms": "$platforms",
+                "youtube_week": "$youtube_charts.week",
+                "youtube_country": "$youtube_charts.country",
+                "youtube_charts": "$youtube_charts.weekly_top_songs"
+            }},
+            # convert yt_rank to integer
+            {"$project": {
+                "_id": 0,
+                "mid": "$mid",
+                "artist": "$artist",
+                "datetime": "$datetime",
+                "year": "$year",
+                "month": "$month",
+                "day": "$day",
+                "week": "$week",
+                "country": "$country",
+                "sp_total_score": "$sp_total_score",
+                "platforms": "$platforms",
+                "youtube_week": "$youtube_week",
+                "youtube_country": "$youtube_country",
+                "yt_charts": {
+                    "$map": {
+                        "input": "$youtube_charts",
+                        "as": "yt_item",
+                        "in": {
+                            "rank": {"$toInt": "$$yt_item.rank"},
+                            "pre_rank": {"$toInt": "$$yt_item.previous_rank"},
+                            "title": "$$yt_item.title",
+                            "artist": "$$yt_item.artist",
+                            "rank_position": "$$yt_item.rank_position"
+                        }
+                    }
+                }
+            }},
+            # calculate youtube charts score by artists first
+            {"$addFields": {
+                "yt_charts": {
+                    "$map": {
+                        "input": "$yt_charts",
+                        "as": "yt_chart",
+                        "in": {
+                            "$mergeObjects": [
+                                "$$yt_chart",
+                                {"yt_score": {
+                                    "$subtract": [
+                                        {"$toInt": 101},
+                                        {"$toInt": "$$yt_chart.rank"}]
+                                }}
+                            ]
+                        }
+                    }
+                }
+            }},
+            {"$unwind": "$yt_charts"},
+            # return expand yt
+            {"$project": {
+                "artist": "$artist",
+                "datetime": "$datetime",
+                "year": "$year",
+                "month": "$month",
+                "day": "$day",
+                "week": "$week",
+                "country": "$country",
+                "sp_total_score": "$sp_total_score",
+                "platforms": "$platforms",
+                "youtube_week": "$youtube_week",
+                "youtube_country": "$youtube_country",
+                "yt_charts_artist": "$yt_charts.artist",
+                "yt_score": "$yt_charts.yt_score"
+            }},
+            {"$group": {
+                "_id": "$yt_charts_artist",
+                #         "_all": {"$addToSet": "$$ROOT"},
+                #             "mid": {"$addToSet": "$mid"},
+                #             "artist": {"$addToSet": "$artist"},
+                #             "sp_total_score": {"$addToSet": "$sp_total_score"},
+                #         "mid": {"$push": "$mid"},
+                #         # artist should be separated
+                #         "artist": {"$push": "$artist"},
+                #         "datetime": {"$first": "$datetime"},
+                #         "year": {"$first": "$year"},
+                #         "month": {"$first": "$month"},
+                #         "day": {"$first": "$day"},
+                #         "week": {"$first": "$week"},
+                #         "country": {"$first": "$country"},
+                #         # spotify score
+                #         "sp_total_score": {"$first": "$sp_total_score"},
+                #         "platforms": {"$first": "$platforms"},
+                #         "youtube_week": {"$first": "$youtube_week"},
+                #         "youtube_country": {"$first": "$youtube_country"},
+                "yt_score": {"$addToSet": "$yt_score"},
+                "yt_artist": {"$first": "$yt_charts_artist"}
+            }}
+
+            # match spotify-youtube artist with artist name
+            #     {"$match": {
+            #        # match artist name with youtube chart artist name
+            #        "artist": "Jimin",
+            #        "youtube_charts": {
+            #           "$elemMatch": {
+            #               "artist": "$youtube_charts.artist"
+            #           }
+            #        }
+            #     }}
+            #     {"$project": {
+            #         "spotify_weekly_top_songs":
+            #     }}
+
+            #     {"$unwind": "$spotify_weekly_top_songs"},
+            #     {"$project": {
+            #         "datetime": "$datetime",
+            #         "country": "$country",
+            #         "year": "$year",
+            #         "month": "$month",
+            #         "day": "$day",
+            #         "week": "$week",
+            #     }}
+            #     {"$project": {
+            #        "combine": {
+            #           "$concatArrays": [
+            #               "$spotify_weekly_top_songs", "$youtube_weekly_top_songs"
+            #            ]
+            #        }
+            #     }}
+            #     {"$unwind": "$youtube_weekly_top_songs"},
+
+        ])
+
+    def get_instagram_latest_twelve(self):
+        pass
+
+
