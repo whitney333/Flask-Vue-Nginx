0b4af3b92f504852525211a35a3de5749980dab3
whitneyliao <whitney304327@gmail.com>
2025-03-26 15:01:19 +0800

    [add]melon controllers

---
+++ backend_rebuild/controllers/melon_controller.py
@@ -0,0 +1,248 @@
+from backend_rebuild.models.melon_model import Melon
+import datetime
+
+
+class MelonController:
+    def get_follower(artist_id, date_end, range):
+        format = "%Y-%m-%d"
+        date_end = datetime.datetime.strptime(date_end, format)
+
+        # Case 1
+        if (range == "7d"):
+            # calculate the date 7 days ago from today
+            seven_days_ago = datetime.datetime.now() - datetime.timedelta(days=7)
+
+            pipeline = [
+                # match artist id in string format
+                {"$match": {
+                    "id": str(artist_id)
+                }},
+                # sort by date
+                {"$sort": {"datetime": 1}},
+                # match date range
+                {"$match": {
+                    "datetime": {
+                        "$lte": date_end,
+                        "$gt": seven_days_ago
+                    }
+                }},
+                # projection
+                {"$project": {
+                    "_id": 0,
+                    "datetime": {
+                        "$dateToString": {
+                            "format": "%Y-%m-%d",
+                            "date": "$datetime"
+                        }
+                    },
+                    "id": "$id",
+                    "follower": "$follower"
+                }}
+            ]
+            results = Melon.objects().aggregate(pipeline)
+
+            result = []
+            for item in results:
+                result.append(item)
+            # print(result)
+
+            return result
+        # Case 2
+        elif (range == "28d"):
+            # calculate the date 28 days ago from today
+            twenty_eight_days_ago = datetime.datetime.now() - datetime.timedelta(days=28)
+
+            pipeline = [
+                # match artist id in string format
+                {"$match": {
+                    "id": str(artist_id)
+                }},
+                # sort by date
+                {"$sort": {"datetime": 1}},
+                # match date range
+                {"$match": {
+                    "datetime": {
+                        "$lte": date_end,
+                        "$gt": twenty_eight_days_ago
+                    }
+                }},
+                # projection
+                {"$project": {
+                    "_id": 0,
+                    "datetime": {
+                        "$dateToString": {
+                            "format": "%Y-%m-%d",
+                            "date": "$datetime"
+                        }
+                    },
+                    "id": "$id",
+                    "follower": "$follower"
+                }}
+            ]
+            results = Melon.objects().aggregate(pipeline)
+
+            result = []
+            for item in results:
+                result.append(item)
+            print(result)
+
+            return result
+        # Case 3
+        elif (range == "90d"):
+            # calculate the date 90 days ago from today
+            ninety_days_ago = datetime.datetime.now() - datetime.timedelta(days=90)
+
+            pipeline = [
+                # match artist id in string format
+                {"$match": {
+                    "id": str(artist_id)
+                }},
+                # sort by date
+                {"$sort": {"datetime": 1}},
+                # match date range
+                {"$match": {
+                    "datetime": {
+                        "$lte": date_end,
+                        "$gt": ninety_days_ago
+                    }
+                }},
+                # projection
+                {"$project": {
+                    "_id": 0,
+                    "datetime": {
+                        "$dateToString": {
+                            "format": "%Y-%m-%d",
+                            "date": "$datetime"
+                        }
+                    },
+                    "id": "$id",
+                    "follower": "$follower"
+                }}
+            ]
+            results = Melon.objects().aggregate(pipeline)
+
+            result = []
+            for item in results:
+                result.append(item)
+            # print(result)
+
+            return result
+        # Case 4
+        elif (range == "180d"):
+            # calculate the date 180 days ago from today
+            hundred_eighty_days_ago = datetime.datetime.now() - datetime.timedelta(days=180)
+
+            pipeline = [
+                # match artist id in string format
+                {"$match": {
+                    "id": str(artist_id)
+                }},
+                # sort by date
+                {"$sort": {"datetime": 1}},
+                # match date range
+                {"$match": {
+                    "datetime": {
+                        "$lte": date_end,
+                        "$gt": hundred_eighty_days_ago
+                    }
+                }},
+                # projection
+                {"$project": {
+                    "_id": 0,
+                    "datetime": {
+                        "$dateToString": {
+                            "format": "%Y-%m-%d",
+                            "date": "$datetime"
+                        }
+                    },
+                    "id": "$id",
+                    "follower": "$follower"
+                }}
+            ]
+            results = Melon.objects().aggregate(pipeline)
+
+            result = []
+            for item in results:
+                result.append(item)
+            # print(result)
+
+            return result
+        # Case 5
+        elif (range == "365d"):
+            # calculate the date 365 days ago from today
+            year_ago = datetime.datetime.now() - datetime.timedelta(days=365)
+
+            pipeline = [
+                # match artist id in string format
+                {"$match": {
+                    "id": str(artist_id)
+                }},
+                # sort by date
+                {"$sort": {"datetime": 1}},
+                # match date range
+                {"$match": {
+                    "datetime": {
+                        "$lte": date_end,
+                        "$gt": year_ago
+                    }
+                }},
+                # projection
+                {"$project": {
+                    "_id": 0,
+                    "datetime": {
+                        "$dateToString": {
+                            "format": "%Y-%m-%d",
+                            "date": "$datetime"
+                        }
+                    },
+                    "id": "$id",
+                    "follower": "$follower"
+                }}
+            ]
+            results = Melon.objects().aggregate(pipeline)
+
+            result = []
+            for item in results:
+                result.append(item)
+            # print(result)
+
+            return result
+        else:
+            # calculate the date 7 days ago from today
+            seven_days_ago = datetime.datetime.now() - datetime.timedelta(days=7)
+
+            pipeline = [
+                # match artist id in string format
+                {"$match": {
+                    "id": str(artist_id)
+                }},
+                # sort by date
+                {"$sort": {"datetime": 1}},
+                # match date range
+                {"$match": {
+                    "datetime": {
+                        "$lte": date_end,
+                        "$gt": seven_days_ago
+                    }
+                }},
+                # projection
+                {"$project": {
+                    "_id": 0,
+                    "datetime": {
+                        "$dateToString": {
+                            "format": "%Y-%m-%d",
+                            "date": "$datetime"
+                        }
+                    },
+                    "id": "$id",
+                    "follower": "$follower"
+                }}
+            ]
+            results = Melon.objects().aggregate(pipeline)
+
+            result = []
+            for item in results:
+                result.append(item)
+            # print(result)
+
+            return result
