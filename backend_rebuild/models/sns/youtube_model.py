18a8ebfed347b39c8460cefc7dde877959656c9b
whitneyliao <whitney304327@gmail.com>
2025-03-26 20:08:49 +0800

    [add]youtube model

---
+++ backend_rebuild/models/sns/youtube_model.py
@@ -0,0 +1,28 @@
+from mongoengine import *
+from datetime import datetime
+
+class YoutubeVideo(EmbeddedDocument):
+    """
+    Embedded field in Youtube document
+    """
+    published_at = DateTimeField(default=datetime.now())
+    title = StringField(required=True)
+    thumbnail = URLField(required=True)
+    code = StringField(required=True, unique=True)
+    tags = ListField(required=True)
+    category_id = StringField(required=True)
+    view_count = StringField(required=True)
+    like_count = IntField(required=True)
+    favorite_count = IntField(required=True)
+    comment_count = IntField(required=True)
+
+class Youtube(Document):
+    datetime = DateTimeField(default=datetime.now())
+    channel_id = StringField(required=True)
+    view_count = StringField(required=True)
+    subscriber_count = IntField(required=True)
+    hidden_subscriber_count = BooleanField(required=True)
+    video_count = IntField(required=True)
+    channel_hashtag = StringField(required=True)
+    video_hashtag = StringField(required=True)
+    video = ListField(EmbeddedDocumentField("YoutubeVideo"))
