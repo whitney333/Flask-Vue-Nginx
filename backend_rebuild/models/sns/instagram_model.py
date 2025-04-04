b2f1e6493a374dde8e558a0824e12b6632bfe4b7
whitneyliao <whitney304327@gmail.com>
2025-04-04 21:00:27 +0800

    [add]instagram basic index model

---
+++ backend_rebuild/models/sns/instagram_model.py
@@ -0,0 +1,12 @@
+from mongoengine import *
+from datetime import datetime
+
+class Instagram(Document):
+    datetime = DateTimeField(default=datetime.now()),
+    pk = StringField(required=True),
+    username = StringField(required=True),
+    media_count = IntField(required=True),
+    follower_count = StringField(required=True),
+    following_count = IntField(required=True),
+    profile_pic = URLField(required=True),
+    thread_follower = IntField(required=True)
