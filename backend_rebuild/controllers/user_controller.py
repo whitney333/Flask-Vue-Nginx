8e107535ab41fc50e4271aba62b1027f30a7e948
whitneyliao <whitney304327@gmail.com>
2025-04-08 17:00:53 +0800

    [add]create user & get single user controller

--- backend_rebuild/controllers/user_controller.py
+++ backend_rebuild/controllers/user_controller.py
@@ -1,12 +1,50 @@
-from models.user_model import User
+from ..models.user_model import User
 import datetime
+from mongoengine import ValidationError, DoesNotExist
 
 class UserController:
-    def create_user(self):
-        pass
+    @staticmethod
+    def create_user(user_data):
+        """
+        create user
+        :return: user object
+        """
+        try:
+            user_data["created_at"] = datetime.datetime.now().isoformat()
+
+            user = User(**user_data)
+            user.save()
+            return user
+        except ValidationError as e:
+            raise ValueError(f"Validation error: {str(e)}")
+        except Exception as e:
+            raise Exception(f"Error creating user: {str(e)}")
 
     def update_user(self):
         pass
 
-    def read_user(self):
-        pass
+    @staticmethod
+    def get_single_user_by_firebase_id(firebase_id):
+        """
+        get user by Firebase ID
+        :return: user object
+        """
+        try:
+            return User.objects.get(firebase_id = firebase_id)
+        except DoesNotExist:
+            return None
+        except Exception as e:
+            raise Exception(f"Error fetching user: {str(e)}")
+
+    @staticmethod
+    def get_single_user_by_email(email):
+        """
+        get user by email
+        :return: user object
+        """
+        try:
+            return  User.objects.get(email=email)
+        except DoesNotExist:
+            return None
+        except Exception as e:
+            raise Exception(f"Error fetching user: {str(e)}")
