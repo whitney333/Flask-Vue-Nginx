85b6b04a12790e040446afaf1102a149f8da5d41
whitneyliao <whitney304327@gmail.com>
2025-04-09 11:22:21 +0800

    [add]instagram threads follower controller

--- backend_rebuild/controllers/sns/instagram_controller.py
+++ backend_rebuild/controllers/sns/instagram_controller.py
@@ -457,6 +457,239 @@ class InstagramController:
 
             return result
 
+    def get_threads_follower(artist_id, date_end, range):
+        """
+        Get Instagram threads follower
+        :param date_end:
+        :param range:
+        :return:
+        """
+        format = "%Y-%m-%d"
+        date_end = datetime.datetime.strptime(date_end, format)
+
+        if (range == "7d"):
+            # calculate the date 7 days ago from today
+            seven_days_ago = date_end - datetime.timedelta(days=7)
+
+            pipeline = [
+                # match artist id
+                {"$match": {
+                    "id": artist_id
+                }},
+                {"$sort": {"datetime": 1}},
+                # match date range
+                {"$match": {
+                    "datetime": {
+                        "$lte": date_end,
+                        "$gt": seven_days_ago
+                    }
+                }},
+                {"$project": {
+                    "_id": 0,
+                    "datetime": {
+                        "$dateToString": {
+                            "format": "%Y-%m-%d",
+                            "date": "$datetime"
+                        }
+                    },
+                    "threads_follower": "$threads_follower"
+                }}
+            ]
+
+            results = Instagram.objects().aggregate(pipeline)
+
+            result = []
+            for item in results:
+                result.append(item)
+            # print(result)
+
+            return result
+        elif (range == "28d"):
+            # calculate the date 28 days ago from today
+            twenty_eight_days_ago = datetime.datetime.now() - datetime.timedelta(days=28)
+
+            pipeline = [
+                # match artist id
+                {"$match": {
+                    "id": artist_id
+                }},
+                {"$sort": {"datetime": 1}},
+                # match date range
+                {"$match": {
+                    "datetime": {
+                        "$lte": date_end,
+                        "$gt": twenty_eight_days_ago
+                    }
+                }},
+                {"$project": {
+                    "_id": 0,
+                    "datetime": {
+                        "$dateToString": {
+                            "format": "%Y-%m-%d",
+                            "date": "$datetime"
+                        }
+                    },
+                    "threads_follower": "$threads_follower"
+                }}
+            ]
+
+            results = Instagram.objects().aggregate(pipeline)
+
+            result = []
+            for item in results:
+                result.append(item)
+            # print(result)
+
+            return result
+        elif (range == "90d"):
+            # calculate the date 90 days ago from today
+            ninety_days_ago = datetime.datetime.now() - datetime.timedelta(days=90)
+
+            pipeline = [
+                # match artist id
+                {"$match": {
+                    "id": artist_id
+                }},
+                {"$sort": {"datetime": 1}},
+                # match date range
+                {"$match": {
+                    "datetime": {
+                        "$lte": date_end,
+                        "$gt": ninety_days_ago
+                    }
+                }},
+                {"$project": {
+                    "_id": 0,
+                    "datetime": {
+                        "$dateToString": {
+                            "format": "%Y-%m-%d",
+                            "date": "$datetime"
+                        }
+                    },
+                    "threads_follower": "$threads_follower"
+                }}
+            ]
+
+            results = Instagram.objects().aggregate(pipeline)
+
+            result = []
+            for item in results:
+                result.append(item)
+            # print(result)
+
+            return result
+        elif (range == "180d"):
+            # calculate the date 180 days ago from today
+            hundred_eighty_days_ago = datetime.datetime.now() - datetime.timedelta(days=180)
+
+            pipeline = [
+                # match artist id
+                {"$match": {
+                    "id": artist_id
+                }},
+                {"$sort": {"datetime": 1}},
+                # match date range
+                {"$match": {
+                    "datetime": {
+                        "$lte": date_end,
+                        "$gt": hundred_eighty_days_ago
+                    }
+                }},
+                {"$project": {
+                    "_id": 0,
+                    "datetime": {
+                        "$dateToString": {
+                            "format": "%Y-%m-%d",
+                            "date": "$datetime"
+                        }
+                    },
+                    "threads_follower": "$threads_follower"
+                }}
+            ]
+
+            results = Instagram.objects().aggregate(pipeline)
+
+            result = []
+            for item in results:
+                result.append(item)
+            # print(result)
+
+            return result
+        elif (range == "365d"):
+            # calculate the date 180 days ago from today
+            year_ago = datetime.datetime.now() - datetime.timedelta(days=365)
+
+            pipeline = [
+                # match artist id
+                {"$match": {
+                    "id": artist_id
+                }},
+                {"$sort": {"datetime": 1}},
+                # match date range
+                {"$match": {
+                    "datetime": {
+                        "$lte": date_end,
+                        "$gt": year_ago
+                    }
+                }},
+                {"$project": {
+                    "_id": 0,
+                    "datetime": {
+                        "$dateToString": {
+                            "format": "%Y-%m-%d",
+                            "date": "$datetime"
+                        }
+                    },
+                    "threads_follower": "$threads_follower"
+                }}
+            ]
+
+            results = Instagram.objects().aggregate(pipeline)
+
+            result = []
+            for item in results:
+                result.append(item)
+            # print(result)
+
+            return result
+        else:
+            # calculate the date 7 days ago from today
+            seven_days_ago = date_end - datetime.timedelta(days=7)
+
+            pipeline = [
+                # match artist id
+                {"$match": {
+                    "id": artist_id
+                }},
+                {"$sort": {"datetime": 1}},
+                # match date range
+                {"$match": {
+                    "datetime": {
+                        "$lte": date_end,
+                        "$gt": seven_days_ago
+                    }
+                }},
+                {"$project": {
+                    "_id": 0,
+                    "datetime": {
+                        "$dateToString": {
+                            "format": "%Y-%m-%d",
+                            "date": "$datetime"
+                        }
+                    },
+                    "threads_follower": "$threads_follower"
+                }}
+            ]
+
+            results = Instagram.objects().aggregate(pipeline)
+
+            result = []
+            for item in results:
+                result.append(item)
+            # print(result)
+
+            return result
+
     def get_posts_index(self):
         pass
     """
