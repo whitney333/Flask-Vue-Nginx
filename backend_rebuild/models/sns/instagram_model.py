fe8c8e8ccf8304072d6b7beef997f1633ad4c382
whitneyliao <whitney304327@gmail.com>
2025-04-24 15:09:56 +0800

    [fix]instagram model name the same as db collection

--- backend_rebuild/models/sns/instagram_model.py
+++ backend_rebuild/models/sns/instagram_model.py
@@ -2,14 +2,33 @@ from mongoengine import *
 from datetime import datetime
 
 class Instagram(Document):
-    datetime = DateTimeField(default=datetime.now()),
-    pk = StringField(required=True),
-    username = StringField(required=True),
-    media_count = IntField(required=True),
-    follower_count = StringField(required=True),
-    following_count = IntField(required=True),
-    profile_pic = URLField(required=True),
+    datetime = DateTimeField(default=datetime.now())
+    user_id = StringField(required=True)
+    username = StringField(required=True)
+    media_count = IntField(required=True)
+    follower_count = StringField(required=True)
+    following_count = IntField(required=True)
+    profile_pic = URLField(required=True)
     thread_follower = IntField(required=True)
 
-class InstagramPost(Document):
-    pass
+class PostDetails(EmbeddedDocument):
+    pk = StringField()
+    id = StringField(required=True)
+    user_pk = StringField(required=True)
+    username = StringField(required=True)
+    code = StringField(required=True)
+    taken_at = DateTimeField(default=datetime.now())
+    media_type = IntField(required=True)
+    product_type = StringField(required=True)
+    comment_count = IntField(required=True)
+    like_count = IntField(required=True)
+    play_count =IntField()
+    view_count = IntField()
+    caption_text = StringField(required=True)
+    thumbnail = URLField(required=True)
+    video_url = URLField(required=True)
+
+class InstagramLatest(Document):
+    datetime = DateTimeField(default=datetime.now())
+    user_id = StringField(required=True)
+    posts = ListField(EmbeddedDocumentField(PostDetails))
