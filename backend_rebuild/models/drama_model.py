85dbf16b348bf9d46841c647158fd3ce9dd25224
whitneyliao <whitney304327@gmail.com>
2025-04-30 13:22:45 +0800

    [refactor]fields name in drama model

--- backend_rebuild/models/drama_model.py
+++ backend_rebuild/models/drama_model.py
@@ -10,6 +10,15 @@ class ProductionName(EmbeddedDocument):
 class StarringId(EmbeddedDocument):
     mid = StringField(required=True)
 
+class PremiereChannel(EmbeddedDocument):
+    name = StringField()
+
+class Screenwriter(EmbeddedDocument):
+    name = StringField()
+
+class Streaming(EmbeddedDocument):
+    name = StringField()
+
 class Drama(Document):
     id = StringField(required=True)
     name = StringField(required=True)
@@ -19,17 +28,16 @@ class Drama(Document):
     broadcast_time = StringField()
     broadcast_year = IntField(required=True)
     country = StringField(required=True)
-    director =
     episode = StringField(required=True)
     special_episode = StringField()
-    finale =
+    finale = DateTimeField()
     genre = ListField(required=True)
     type = StringField(required=True)
     language = StringField(required=True)
-    premiere_channel =
-    streaming =
-    director =
-    production =
-    screenwriter =
-    starring =
+    premiere_channel = ListField(EmbeddedDocumentField(PremiereChannel))
+    streaming = ListField(EmbeddedDocumentField(Streaming))
+    director = ListField(EmbeddedDocumentField(DirectorName))
+    production = ListField(EmbeddedDocumentField(ProductionName))
+    screenwriter = ListField(EmbeddedDocumentField(Screenwriter))
+    starring = ListField(EmbeddedDocumentField(StarringId))
     image = URLField()
