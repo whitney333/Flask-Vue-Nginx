aec34eab4f55cb2af79f5466b3b914b48d1388c8
whitneyliao <whitney304327@gmail.com>
2025-08-18 16:31:14 +0800

    [refactor]change field name from id to melon_id in melon collection & change melon_id from referenceField to stringField in artist model

--- backend_rebuild/models/artist_model.py
+++ backend_rebuild/models/artist_model.py
@@ -1,27 +1,65 @@
 from mongoengine import *
 from datetime import datetime
+from .tenant_model import Tenant
+from models.sns.instagram_model import Instagram
+from models.sns.youtube_model import Youtube
+from models.sns.tiktok_model import Tiktok
+from models.sns.bilibili_model import Bilibili
+from models.melon_model import Melon
+from models.spotify_model import Spotify
+from enum import Enum
 
+class Nation(Enum):
+    # nation list for selection
+    Taiwan = "TW"
+    Hong_Kong = "HK"
+    Japan = "JP"
+    South_Korea = "KR"
+    Thailand = "TH"
+    Vietnam = "VN"
+    Philippines = "PH"
+    Indonesia = "ID"
+    United_States = "US"
+    Canada = "CA"
+    Brazil = "BR"
+    Mexico = "MX"
+    United_Kingdom = "GB"
+    Germany = "DE"
+    France = "FR"
+    Spain = "ES"
+    Italy = "IT"
+    Australia = "AU"
 
 class Artists(Document):
+    tenant_id = ReferenceField(Tenant, required=True)
     artist_id = StringField(required=True)
     english_name = StringField(required=True)
     korean_name = StringField(required=True)
-    debut_year = StringField()
+    debut_year = IntField()
     nation = StringField(required=True)
     pronouns = StringField(required=True)
     type = ListField(required=True)
     birth = DateTimeField()
     fandom = StringField()
     belong_group = ListField()
-    instagram_id = StringField()
-    instagram_user = StringField()
     threads = BooleanField()
-    youtube_id = StringField()
-    tiktok_id = StringField()
-    spotify_id = StringField()
-    melon_id = StringField()
+    instagram_id = ReferenceField("Instagram")
+    youtube_id = StringField("Youtube")
+    tiktok_id = ReferenceField("Tiktok")
+    spotify_id = ReferenceField("Spotify")
+    melon_id = StringField("Melon")
     genie_id = StringField()
-    bilibili_id = StringField()
-    qq_id = StringField()
+    bilibili_id = ReferenceField("Bilibili")
     weibo_id = StringField()
-    image = URLField()
+    image_url = URLField()
+    # instagram_id = StringField()
+    instagram_user = StringField()
+    # youtube_id = StringField()
+    # tiktok_id = StringField()
+    # spotify_id = StringField()
+    # melon_id = StringField()
+    # genie_id = StringField()
+    # bilibili_id = StringField()
+    # qq_id = StringField()
+    # weibo_id = StringField()
+
