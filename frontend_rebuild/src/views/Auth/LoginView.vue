3b4da7e5bf7d5b8d8c2017610e87fe503ce62151
whitneyliao <whitney304327@gmail.com>
2025-09-24 13:45:39 +0800

    [fix]remove layout

--- frontend_rebuild/src/views/Auth/LoginView.vue
+++ frontend_rebuild/src/views/Auth/LoginView.vue
@@ -63,7 +63,7 @@
               email: result.user.email,
               name: result.user.displayName,
               photo: result.user.photoURL,
-              firebaseToken: result.user.accessToken
+              firebaseToken: idToken
             })
 
           // POST firebase_id to check if user exists
@@ -72,24 +72,23 @@
               {firebase_id: result.user.uid},
               {
                 headers: {
-                  Authorization: `Bearer ${result.user.accessToken}`
+                  Authorization: `Bearer ${idToken}`
                 }
               }
           );
-            console.log("resp: ", response)
+
           // const token = response.data;
           // if data exists then return
           const {exists} = response.data
-          console.log("resp: ", response.data)
 
           // get followed artists list
           if (exists === true) {
             const getFollowedArtists = await axios.get(
-                "/user/v1/followed_artists", {
-              headers: {
-                Authorization: `Bearer ${result.user.accessToken}`,
-                timeout: 10000
-              }
+                "/user/v1/followed_artists",
+                {headers: {
+                Authorization: `Bearer ${idToken}`,
+              },
+              timeout: 10000
             });
             // store followed artist
             userStore.setFollowedArtists(getFollowedArtists.data.data || []);
@@ -159,17 +158,17 @@
             // const token = response.data;
             // if data exists then return
             const { exists } = response.data
-            // console.log("resp: ", response.data)
+            console.log("resp: ", response.data)
 
             // get followed artists list
             if (exists === true) {
               const getFollowedArtists = await axios.get(
-                  "/user/v1/followed_artists", {
-                headers: {
-                Authorization: `Bearer ${idToken}`,
-                timeout: 10000
-                }
-              });
+                  "/user/v1/followed_artists",
+                  {headers: {
+                     Authorization: `Bearer ${idToken}`,
+                  }}
+              );
+
               // store followed artist
               userStore.setFollowedArtists(getFollowedArtists.data.data || []);
               console.log("Followed Artists in store:", userStore.followedArtists);
