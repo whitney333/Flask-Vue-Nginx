a51d09d80f626275531f530a453e352e9459bd4e
whitneyliao <whitney304327@gmail.com>
2025-09-30 10:57:11 +0800

    [fix]center icons & text in mobile version

--- frontend_rebuild/src/views/Campaign/Coming_SoonView.vue
+++ frontend_rebuild/src/views/Campaign/Coming_SoonView.vue
@@ -3,28 +3,21 @@
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-4">
-    <div class="relative text-8xl mb-6 animate-bounce-slow">
-      🚀
+  <div class="flex items-center justify-center min-h-dvh bg-gray-50 p-4">
+    <div class="flex flex-col items-center text-center">
+      <div class="relative text-8xl mb-6 animate-bounce-slow">
+        🚀
+      </div>
+      <h1 class="text-5xl font-bold tracking-widest">
+        COMING SOON
+      </h1>
+      <p class="mt-3 text-gray-400 text-lg"> This feature is under development. Stay tuned for updates! </p>
+      <p class="mt-3 text-gray-400 text-lg"> We’re launching something exciting! </p>
+      <router-link to="/dashboard"
+                   class="mt-6 px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
+        Back to Dashboard
+      </router-link>
     </div>
-    <h1 class="text-5xl font-bold tracking-widest">COMING SOON</h1>
-    <!-- Title -->
-<!--    <h1 class="text-4xl font-bold mb-4 text-gray-800 text-center">Coming Soon</h1>-->
-
-    <!-- Subtitle -->
-    <p class="mt-3 text-gray-400 text-lg">
-      This feature is under development. Stay tuned for updates!
-    </p>
-    <p class="mt-3 text-gray-400 text-lg">
-      We’re launching something exciting!
-    </p>
-    <!-- Optional button to go back -->
-    <router-link
-        to="/dashboard"
-        class="mt-3 px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
-    >
-      Back to Dashboard
-    </router-link>
   </div>
 </template>
 
