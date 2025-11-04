be598bc1c48d09eaac3c1f223b349ae14b7075ac
whitneyliao <whitney304327@gmail.com>
2025-11-04 15:17:44 +0800

    [update]

--- frontend_rebuild/src/main.js
+++ frontend_rebuild/src/main.js
@@ -14,7 +14,6 @@ import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
 import i18n from './i18n'; // Path to your i18n setup file
 import './assets/tailwind.css'; // Import Tailwind
-import {createHead} from '@unhead/vue/client'
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
 import "/node_modules/ag-grid-community/styles/ag-theme-quartz.css";
 // window.Apex.chart = { fontFamily: "Cairo, sans-serif" };
@@ -28,8 +27,6 @@ firebase()
 const app = createApp(App);
 const pinia = createPinia();
 
-const head = createHead();
-
 // register ag-grid
 ModuleRegistry.registerModules([AllCommunityModule]);
 
@@ -39,7 +36,6 @@ app.use(router);
 app.use(vuetify);
 app.use(VueApexCharts);
 app.use(i18n);
-app.use(head);
 app.use(pinia);
 
 app.mount('#app');
