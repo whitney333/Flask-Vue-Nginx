72863897a5a296b13385b428933d4809165999c0
whitneyliao <whitney304327@gmail.com>
2026-01-06 16:18:58 +0800

    [add]weibo controller & service .py

---
+++ backend_rebuild/services/weibo_service.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from models.sns.weibo_model import Weibo
+
+class WeiboService:
+
+    @staticmethod
+    def get_follower_growth(weibo_id, campaign_start):
+        """
+        Get Weibo follower growth for a campaign
+        (campaign_start -14d) → (campaign_start +14d)
+        :param spotify_id:
+        :param campaign_start:
+        :return:
+        """
+        pass
+
+
