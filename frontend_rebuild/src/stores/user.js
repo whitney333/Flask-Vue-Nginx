88d83844267a38e1f24889d153172d14be4abdf7
whitneyliao <whitney304327@gmail.com>
2026-02-09 18:05:52 +0800

    [fix]remove token stored in user.js

--- frontend_rebuild/src/stores/user.js
+++ frontend_rebuild/src/stores/user.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import { getAuth } from "firebase/auth"
 
 // store user token in pinia
 export const useUserStore = defineStore("user", {
@@ -13,21 +14,59 @@ export const useUserStore = defineStore("user", {
     admin: null,
     created_at: null,
     last_login_at: null,
-    isPremium: null
+    isPremium: null,
+    plan: null,
+    expiredAt: null
   }),
   actions: {
+    // get user state in backend
+    async fetchMe() {
+      const userStore = useUserStore()
+      const auth = getAuth()
+      const user = auth.currentUser
+
+      if (!user) {
+        console.log("[userStore] fetchMe skipped: no user logged in")
+        return
+      }
+      // refresh token
+      const idToken = await user.getIdToken(true)
+
+      try {
+        const res = await fetch("/api/user/me", {
+          headers: {
+            Authorization: `Bearer ${idToken}`
+          }
+        })
+
+        if (!res.ok) {
+           new Error("fetchMe failed")
+        }
+
+        const user = await res.json()
+        this.setUser(user)
+      } catch (err) {
+        console.error("[userStore] fetchMe error:", err)
+      }
+    },
+    // update user state
     setUser(user) {
       this.firebase_id = user.firebase_id;
       this.email = user.email;
       this.name = user.name;
       this.photo = user.photo;
       this.tenant = user.tenant;
-      this.followedArtists = user.followedArtists || [];
-      this.firebaseToken = user.firebaseToken;
+      if (Array.isArray(user.followedArtists)) {
+        this.followedArtists = user.followedArtists;
+      }
       this.admin = user.admin;
       this.created_at = user.created_at;
       this.last_login_at = user.last_login_at;
-      this.isPremium = !!user.is_premium
+      this.isPremium = !!user.is_premium;
+      this.plan = user.plan;
+      if (user.expired_at !== undefined) {
+        this.expiredAt = user.expired_at;
+      }
     },
     reset() {
       this.firebase_id = null
@@ -36,14 +75,15 @@ export const useUserStore = defineStore("user", {
       this.photo = null
       this.tenant = null
       this.followedArtists = []
-      this.firebaseToken = null
       this.admin = null
       this.created_at = null
       this.last_login_at = null
       this.isPremium = false
+      this.plan = null
+      this.expiredAt = null
     },
     setFollowedArtists(artists) {
-      // console.log("Setting followedArtists in store:", artists);
+      console.log("Setting followedArtists in store:", artists);
       this.followedArtists = artists || [];
     },
     removeFollowedArtist(artistId) {
