7696fc26bd6be0f86e4459e8730ee08dad0d3b0e
whitneyliao <whitney304327@gmail.com>
2026-05-26 10:16:11 +0800

    [feat]integrate trending-artists popularity api and adjust table styling

--- frontend_rebuild/src/views/TrendingArtists/TrendingArtistsView.vue
+++ frontend_rebuild/src/views/TrendingArtists/TrendingArtistsView.vue
@@ -3,251 +3,345 @@ import { computed, onMounted, ref, watch } from 'vue';
 import getUnicodeFlagIcon from 'country-flag-icons/unicode'
 import axios from '@/axios';
 import TACard from '@/views/TrendingArtists/components/TA_card.vue';
-const countriesFlag = {
-    'Global': 'UN',
-    'Taiwan': 'TW',
-    'Hong Kong': 'HK',
-    'Japan': 'JP',
-    'South Korea': 'KR',
-    'Thailand': 'TH',
-    'Vietnam': 'VN',
-    'Philippines': 'PH',
-    'Indonesia': 'ID',
-    'United States': 'US',
-    'Canada': 'CA',
-    'Brazil': 'BR',
-    'Mexico': 'MX',
-    'United Kingdom': 'GB',
-    'Germany': 'DE',
-    'France': 'FR',
-    'Spain': 'ES',
-    'Italy': 'IT',
-    'Australia': 'AU'
-}
-const selectCountry = ref({
-        title: `${getUnicodeFlagIcon(countriesFlag['Global'])} ${'Global'}`,
-        value: 'Global',
-    })
-const countries = ref([
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['Global'])} ${'Global'}`,
-        value: 'Global',
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['Taiwan'])} ${'Taiwan'}`,
-        value: 'Taiwan',
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['Hong Kong'])} ${'Hong Kong'}`,
-        value: 'Hong Kong',
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['Japan'])} ${'Japan'}`,
-        value: 'Japan',
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['South Korea'])} ${'South Korea'}`,
-        value: 'South Korea',
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['Thailand'])} ${'Thailand'}`,
-        value: 'Thailand',
-        
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['Vietnam'])} ${'Vietnam'}`,
-        value: 'Vietnam',
-        
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['Philippines'])} ${'Philippines'}`,
-        value: 'Philippines',
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['Indonesia'])} ${'Indonesia'}`,
-        value: 'Indonesia',
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['United States'])} ${'United States'}`,
-        value: 'United States',
-        
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['Canada'])} ${'Canada'}`,
-        value: 'Canada',
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['Brazil'])} ${'Brazil'}`,
-        value: 'Brazil',
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['Mexico'])} ${'Mexico'}`,
-        value: 'Mexico',
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['United Kingdom'])} ${'United Kingdom'}`,
-        value: 'United Kingdom',
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['Germany'])} ${'Germany'}`,
-        value: 'Germany',
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['France'])} ${'France'}`,
-        value: 'France',
-        
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['Spain'])} ${'Spain'}`,
-        value: 'Spain',
-        
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['Italy'])} ${'Italy'}`,
-        value: 'Italy',
-        
-    },
-    {
-        title: `${getUnicodeFlagIcon(countriesFlag['Australia'])} ${'Australia'}`,
-        value: 'Australia',
-    } 
-])
-const selectType = ref('All')
-const types = ref(['All', 'Actor', 'Musician'])
-const artistList = ref([
-    {
-        artistId: 15,
-        artistName: 'Byeon WooSeok',
-        rank: 1,
-        icon: 'https://mishkan-ltd.s3.ap-northeast-2.amazonaws.com/web-img/twitter-logo.svg',
-        type: 'Actor',
-        popularity: 78426,
 
-    },
-    {
-        artistId: 2,
-        artistName: 'Kim SooHyun',
-        rank: 2,
-        icon: 'https://mishkan-ltd.s3.ap-northeast-2.amazonaws.com/web-img/twitter-logo.svg',
-        type: 'Actor',
-        popularity: 72391
-    },
-    {
-        artistId: 44,
-        artistName: 'Kim JiWon',
-        rank: 3,
-        icon: 'https://mishkan-ltd.s3.ap-northeast-2.amazonaws.com/web-img/twitter-logo.svg',
-        type: 'Actor',
-        popularity: 70514
-    },
-    {
-        artistId: 10,
-        artistName: 'Kim HyeYoon',
-        rank: 4,
-        icon: 'https://mishkan-ltd.s3.ap-northeast-2.amazonaws.com/web-img/twitter-logo.svg',
-        type: 'Actor',
-        popularity: 69685
-    },
-    {
-        artistId: 55,
-        artistName: 'NewJeans',
-        rank: 5,
-        icon: 'https://mishkan-ltd.s3.ap-northeast-2.amazonaws.com/web-img/twitter-logo.svg',
-        type: 'Musician',
-        popularity: 68125
+  const countriesFlag = {
+      'Global': 'UN',
+      'Taiwan': 'TW',
+      'Hong Kong': 'HK',
+      'Japan': 'JP',
+      'South Korea': 'KR',
+      'Thailand': 'TH',
+      'Vietnam': 'VN',
+      'Philippines': 'PH',
+      'Indonesia': 'ID',
+      'United States': 'US',
+      'Canada': 'CA',
+      'Brazil': 'BR',
+      'Mexico': 'MX',
+      'United Kingdom': 'GB',
+      'Germany': 'DE',
+      'France': 'FR',
+      'Spain': 'ES',
+      'Italy': 'IT',
+      'Australia': 'AU'
+  }
+
+  const selectCountry = ref({
+          title: `${getUnicodeFlagIcon(countriesFlag['Global'])} ${'Global'}`,
+          value: 'global',
+      })
+  const countries = ref([
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['Global'])} ${'Global'}`,
+          value: 'global',
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['Taiwan'])} ${'Taiwan'}`,
+          value: 'tw',
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['Hong Kong'])} ${'Hong Kong'}`,
+          value: 'hk',
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['Japan'])} ${'Japan'}`,
+          value: 'jp',
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['South Korea'])} ${'South Korea'}`,
+          value: 'kr',
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['Thailand'])} ${'Thailand'}`,
+          value: 'th',
+
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['Vietnam'])} ${'Vietnam'}`,
+          value: 'vn',
+
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['Philippines'])} ${'Philippines'}`,
+          value: 'ph',
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['Indonesia'])} ${'Indonesia'}`,
+          value: 'id',
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['United States'])} ${'United States'}`,
+          value: 'us',
+
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['Canada'])} ${'Canada'}`,
+          value: 'ca',
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['Brazil'])} ${'Brazil'}`,
+          value: 'br',
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['Mexico'])} ${'Mexico'}`,
+          value: 'mx',
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['United Kingdom'])} ${'United Kingdom'}`,
+          value: 'gb',
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['Germany'])} ${'Germany'}`,
+          value: 'de',
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['France'])} ${'France'}`,
+          value: 'fr',
+
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['Spain'])} ${'Spain'}`,
+          value: 'es',
+
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['Italy'])} ${'Italy'}`,
+          value: 'it',
+
+      },
+      {
+          title: `${getUnicodeFlagIcon(countriesFlag['Australia'])} ${'Australia'}`,
+          value: 'au',
+      }
+  ])
+  const selectType = ref('All')
+  const types = ref(['All', 'Actor', 'Musician'])
+  const artistList = ref([])
+  const thisYear = new Date().getFullYear()
+  const currentYear = ref(thisYear)
+
+  // ISO week
+  const getWeekNumber = () => {
+      const date = new Date()
+      const target = new Date(date.valueOf())
+      const dayNr = (date.getDay() + 6) % 7
+
+      target.setDate(target.getDate() - dayNr + 3)
+
+      const firstThursday = target.valueOf()
+
+      target.setMonth(0, 1)
+
+      if (target.getDay() !== 4) {
+          target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7)
+      }
+
+      return Math.ceil((firstThursday - target) / 604800000)
+  }
+
+  const thisWeek = getWeekNumber()
+  const currentWeek = ref(thisWeek)
+  const yearOptions = computed(() => {
+      return Array.from({ length: thisYear - 2020 + 1 }, (_, i) => thisYear - i)
+  })
+  const weekOptions = computed(() => {
+      const maxWeek = currentYear.value === thisYear ? thisWeek : 53
+      return Array.from({ length: maxWeek }, (_, i) => i + 1)
+  })
+
+  const artistPresentList = computed(() => {
+    if (selectType.value == 'Actor') {
+      return artistList.value.filter((x) => x.type == 'Actor')
+    } else if (selectType.value == 'Musician') {
+      return artistList.value.filter((x) => x.type == 'Musician')
+    } else {
+      return artistList.value
     }
+    // todo Region
+  })
 
-])
-    const artistPresentList = computed(() => {
-        if (selectType.value == 'Actor') {
-            return artistList.value.filter((x) => x.type == 'Actor')
-        } else if (selectType.value == 'Musician') {
-            return artistList.value.filter((x) => x.type == 'Musician')
-        } else {
-            return artistList.value
-        }
-        // todo Region
-    })
+    const normalizeArtists = (payload) => {
+      const artists = payload?.artists || payload?.data || payload || []
+
+      if (!Array.isArray(artists)) {
+        return []
+      }
+
+      return artists.map((artist) => ({
+        ...artist,
+        artistId: artist.artistId ?? artist.artist_id,
+        artistName: artist.artistName ?? artist.english_name ?? '',
+        artistKoreanName: artist.artistKoreanName ?? artist.korean_name ?? '',
+        artistImg: artist.image ?? artist.image_url ?? '',
+        popularity: artist.popularity ?? artist.popularity_score ?? 0,
+      }))
+    }
 
     // fetch Artist List everytime the country changed
     const fetchArtistList = async () => {
-        const data = await axios.get(`trending-artist/rank/${selectCountry.value.split(" ").join('').toLowerCase()}`)
-        artistList.value = data
+      try {
+        const response = await axios.get('/trending-artist/v2/popularity', {
+          params: {
+            year: currentYear.value,
+            week: currentWeek.value,
+            country: selectCountry.value.value
+          }
+        })
+
+        artistList.value = normalizeArtists(response.data)
+
+      } catch (error) {
+        console.error('Fetch trending artist failed:', error)
+        artistList.value = []
+      }
     }
-    
-    // watch(selectCountry, fetchArtistList)
+
+    watch(currentYear, () => {
+      const maxWeek = currentYear.value === thisYear ? thisWeek : 53
+
+      if (currentWeek.value > maxWeek) {
+        currentWeek.value = maxWeek
+      }
+    })
+
+    watch([selectCountry, currentYear, currentWeek], fetchArtistList)
+
+    onMounted(() => {
+      fetchArtistList()
+    })
 </script>
 
 <template>
-    <v-container
-    fluid
-    :class="['bg-grey-lighten-4', 'fill-height', 'align-start']"
-    >
-    <v-container
-    >
-        <div :class="['d-flex', 'justify-end', 'align-center', 'ga-5']">
-            <!-- Region Selection -->
-                <v-select
-                    bg-color="#FFFFFF"
-                    :minWidth="100"
-                    :maxWidth="200"
-                    label="Standard"
-                    :items="countries"
-                    variant="outlined"
-                    single-line
-                    density="compact"
-                    return-object
-                    v-model="selectCountry"
-                    item-title="title"
-                    rounded
-                >
-                </v-select>
-                <v-select
-                    bg-color="#FFFFFF"
+    <div class="min-h-screen bg-gray-100 w-full">
+        <div class="max-w-7xl mx-auto px-4 py-6">
 
-                    :minWidth="100"
-                    :maxWidth="200"
-                    rounded
-                    label="Type"
-                    :items="types"
-                    variant="outlined"
-                    density="compact"
-                    single-line
-                    return-object
-                    v-model="selectType"
-                    item-title="region"
-                >
-                    <template v-if="selectType == 'All'" v-slot:prepend-inner>
-                        <v-icon :icon="'mdi-view-dashboard'" />
-                    </template>
-                </v-select>
+            <!-- Top Filter Bar -->
+            <div
+                class="bg-white rounded-2xl shadow-sm border border-gray-200 p-4 mb-6"
+            >
+                <div class="flex flex-wrap justify-end gap-4">
+
+                    <div class="w-full sm:w-[180px]">
+                        <v-select
+                            bg-color="#FFFFFF"
+                            label="Standard"
+                            :items="countries"
+                            variant="outlined"
+                            density="compact"
+                            single-line
+                            rounded
+                            return-object
+                            v-model="selectCountry"
+                            item-title="title"
+                            hide-details
+                        />
+                    </div>
+
+                    <div class="w-full sm:w-[140px]">
+                        <v-select
+                            bg-color="#FFFFFF"
+                            label="Year"
+                            :items="yearOptions"
+                            variant="outlined"
+                            density="compact"
+                            single-line
+                            rounded
+                            hide-details
+                            v-model="currentYear"
+                        />
+                    </div>
+
+                    <div class="w-full sm:w-[140px]">
+                        <v-select
+                            bg-color="#FFFFFF"
+                            label="Week"
+                            :items="weekOptions"
+                            variant="outlined"
+                            density="compact"
+                            single-line
+                            rounded
+                            hide-details
+                            v-model="currentWeek"
+                        />
+                    </div>
+
+                    <div class="w-full sm:w-[180px]">
+                        <v-select
+                            bg-color="#FFFFFF"
+                            label="Type"
+                            :items="types"
+                            variant="outlined"
+                            density="compact"
+                            single-line
+                            rounded
+                            hide-details
+                            return-object
+                            v-model="selectType"
+                            item-title="region"
+                        >
+                            <template
+                                v-if="selectType == 'All'"
+                                v-slot:prepend-inner
+                            >
+                                <v-icon icon="mdi-view-dashboard" />
+                            </template>
+                        </v-select>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Table -->
+            <div
+                class="bg-white rounded-3xl shadow-sm border border-gray-200 overflow-hidden"
+            >
+
+              <!-- Header -->
+              <div
+                  class="
+                  hidden md:grid
+                  grid-cols-12 items-center
+                  px-6 py-4
+                  bg-gray-50 border-b border-gray-200
+                  text-sm font-semibold text-gray-600
+                  uppercase tracking-wide
+                  "
+              >
+                <div class="col-span-1 text-center">
+                  {{ $t('Rank') }}
+                </div>
+
+                <div class="col-span-4">
+                  {{ $t('Artist') }}
+                </div>
+
+                <div class="col-span-2">
+                  {{ $t('Type') }}
+                </div>
+
+                <div class="col-span-2">
+                  {{ $t('Popularity') }}
+                </div>
+
+                <div class="col-span-3">
+                  {{ $t('Scores') }}
+                </div>
+
+                <div class="col-span-1"></div>
+              </div>
+
+                <!-- Rows -->
+                <div class="divide-y divide-gray-100">
+                    <TACard
+                        v-for="(artist, i) in artistPresentList"
+                        :key="i"
+                        :value="artist"
+                    />
+                </div>
+            </div>
         </div>
-        <v-row
-        :class="['px-3']">
-            <v-col
-            cols="1">
-            <span :class="['font-weight-medium', 'text-body-1']">{{ $t('Rank') }}</span>
-            </v-col>
-            <v-col
-            cols="6">
-            <span :class="['font-weight-medium', 'text-body-1']">{{ $t('Artist') }}</span>
-            </v-col>
-            <v-col
-            cols="2">
-            <span :class="['font-weight-medium', 'text-body-1']">{{ $t('Popularity') }}</span>
-            </v-col>
-            <v-col
-            cols="3">
-            <span :class="['font-weight-medium', 'text-body-1']">{{ $t('7-day Change') }}</span>
-            </v-col>
-        </v-row>
-        <TACard v-for="(artist, i) in artistPresentList" :value="artist" :key="i">
-
-        </TACard>
-    </v-container>
-
-    </v-container>
+    </div>
 </template>
+
 <style scoped>
     .selection-appearence {
         border-radius: 20% !important;
