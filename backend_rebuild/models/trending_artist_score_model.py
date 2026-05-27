33425c6a705724467cb0a135309fbe4fc115d198
whitneyliao <whitney304327@gmail.com>
2026-05-27 09:46:32 +0800

    [feat(model)]add rank field to trending artist score schema

--- backend_rebuild/models/trending_artist_score_model.py
+++ backend_rebuild/models/trending_artist_score_model.py
@@ -19,25 +19,28 @@ class ArtistPopularity(Document):
     drama_score = FloatField(default=0)
 
     popularity_score = FloatField(default=0)
-
+    rank = IntField(min_value=1)
     updated_at = DateTimeField(default=datetime.utcnow)
     created_at = DateTimeField(default=datetime.utcnow)
 
     meta = {
         "collection": "artist_popularity",
         "indexes": [
-            ("country", "year", "week", "-overall_score"),
-            ("country", "year", "week", "-music_score"),
-            ("country", "year", "week", "-sns_score"),
-            ("country", "year", "week", "-drama_score"),
-            ("artist_id", "country", "year", "week"),
-            {"fields": [
-                    "artist_id",
-                    "country",
-                    "year",
-                    "week"
-                ],
+            {
+                "fields": ["artist_id", "country", "year", "week"],
                 "unique": True
+            },
+            {
+                "fields": ["country", "year", "week", "-popularity_score"]
+            },
+            {
+                "fields": ["country", "year", "week", "-music_score"]
+            },
+            {
+                "fields": ["country", "year", "week", "-sns_score"]
+            },
+            {
+                "fields": ["country", "year", "week", "-drama_score"]
             }
         ]
     }
