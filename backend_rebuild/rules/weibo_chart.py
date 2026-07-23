88618f3445e971457f4d909f40eb64f18aa241de
whitneyliao <whitney304327@gmail.com>
2026-07-23 18:26:34 +0800

    [feat]add Weibo validation rules

---
+++ backend_rebuild/rules/weibo_chart.py
@@ -0,0 +1,11 @@
+FOLLOWER_RANGE_RULES = {
+    "free": ["28d"],
+    "premium": ["28d", "90d", "180d", "365d"]
+}
+
+RANGE_DAYS = {
+    "28d": 28,
+    "90d": 90,
+    "180d": 180,
+    "365d": 365
+}
