2e6dcb27209d3f554255089db9b36c61eab9061f
whitneyliao <whitney304327@gmail.com>
2026-08-20 17:21:26 +0800

    [feat(model)]add rank_change/rank_type/previous_rank to trending artist score model

--- backend_rebuild/models/trending_artist_score_model.py
+++ backend_rebuild/models/trending_artist_score_model.py
@@ -20,6 +20,10 @@ class ArtistPopularity(Document):
 
     popularity_score = FloatField(default=0)
     rank = IntField(min_value=1)
+    previous_rank = IntField(min_value=1, default=None)
+    rank_change = IntField(default=0)
+    rank_type = StringField(default="NEW")
+
     updated_at = DateTimeField(default=datetime.utcnow)
     created_at = DateTimeField(default=datetime.utcnow)
 
